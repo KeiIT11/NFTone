0f378bd5a57b12b3caf563d27de7a63e286c2153
akiogitgit <akimaron2828@icloud.com>
2023-03-12 23:13:57 +0900

    feat: フォームの実行処理を、呼び出す側で決める

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import { Layout } from '@/components/Layout'
-import { Form } from '@/components/form'
+import { Answers, Form } from '@/components/form'
+import { questions } from '@/constants/question'
 import { Center } from '@mantine/core'
+import { useCallback } from 'react'
+
+type QuestionResults = { question: string; value: number }[]
 
 export default function Home() {
+  const onSubmit = useCallback((e: Answers) => {
+    // 質問と結果をマージ ex) [{question: "あなたは？", value: 5}, ...]
+    const res: QuestionResults = e.answers.map((v, i) => ({
+      question: questions[i],
+      value: v,
+    }))
+
+    console.log(res)
+  }, [])
+
   return (
     <Layout title='Topページ'>
       <Center>
-        <Form />
+        <Form onSubmit={onSubmit} />
       </Center>
     </Layout>
   )
