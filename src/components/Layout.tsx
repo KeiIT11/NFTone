c4d584baf5bd0d5651f7d117e788f2581a3695ac
akiogitgit <akimaron2828@icloud.com>
2023-03-13 16:28:01 +0900

    feat: ヘッダーをNFTのように変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,15 @@
 import { FC, ReactNode } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
 
 type Props = {
   children: ReactNode
   title?: string
 }
 
-const menus = [
-  { path: '/', label: 'ホーム' },
-  { path: '/about', label: '私たちについて' },
-]
-
 export const Layout: FC<Props> = ({
   title = 'nft-personality-test',
   children,
 }) => {
-  const router = useRouter()
-
   return (
     <div className='min-h-screen bg-gray-100 overflow-hidden'>
       <Head>
@@ -28,32 +18,16 @@ export const Layout: FC<Props> = ({
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <header className='bg-blue-500 shadow-lg w-full py-2'>
-        <nav className='flex mx-5 items-center justify-between'>
-          <h1 className='cursor-pointer flex mt-0 mb-1 px-2 transform text-2xl items-center'>
-            <Image src='/favicon.ico' height={50} width={50} alt='' />
-            <span className='font-bold ml-3'>nft-personality-test</span>
+      <header className='bg-black shadow-md shadow-black w-full'>
+        <nav className='text-center'>
+          <h1 className='cursor-pointer text-4xl font-bold py-20 bg-gradient-to-r bg-clip-text font-bold from-red-600 via-violet-400 to-blue-600 text-transparent'>
+            NFT Personality Test
           </h1>
-          <div className='hidden md:(flex gap-3) '>
-            {menus.map(menu => (
-              <Link href={menu.path} key={menu.path}>
-                <div className={`font-bold text-white text-sm group`}>
-                  {menu.label}
-                  <div
-                    className={`
-                    ${
-                      router.pathname === menu.path ? 'w-full' : 'w-0'
-                    } bg-white h-1px mt-1 duration-150 group-hover:w-full`}
-                  ></div>
-                </div>
-              </Link>
-            ))}
-          </div>
         </nav>
       </header>
 
       <main>
-        <div className='mx-auto max-w-900px py-10 px-4'>{children}</div>
+        <div className='mx-auto max-w-900px my-30 px-4'>{children}</div>
       </main>
     </div>
   )
